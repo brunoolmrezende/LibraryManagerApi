13b6301f33c06320be0ef73b252396cfe5bf397b
brunoolmrezende <brunoolmrezende303@gmail.com>
2024-05-02 17:48:48 -0300

    Add GenreName

--- Book.cs
+++ Book.cs
@@ -6,6 +6,7 @@ public class Book
     public string Title { get; set; } = string.Empty;
     public string Author { get; set; } = string.Empty;
     public BookGenre Genre { get; set; }
+    public string GenreName => Enum.GetName(typeof(BookGenre), Genre);
     public double Price { get; set; }
     public int Quantity { get; set; }
 }
--- Communication/Responses/ResponseRegisteredBookJson.cs
+++ Communication/Responses/ResponseRegisteredBookJson.cs
@@ -5,6 +5,7 @@ public class ResponseRegisteredBookJson
     public string Title { get; set; } = string.Empty;
     public string Author { get; set; } = string.Empty;
     public BookGenre Genre { get; set; }
+    public string GenreName => Enum.GetName(typeof(BookGenre), Genre);
     public double Price { get; set; }
     public int Quantity { get; set; }
 }
